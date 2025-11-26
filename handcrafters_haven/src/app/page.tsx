8943793a25f6210b9c7e1f2e2c9f61391905ecbc
hieusStorm <hyrum555@gmail.com>
2025-11-25 17:23:16 -0700

    made cart layout

--- handcrafters_haven/src/app/page.tsx
+++ handcrafters_haven/src/app/page.tsx
@@ -15,70 +15,8 @@ async function getProducts() {
 
  export default async function Home() {
   const products = await getProducts();
-  return (
-    /*<div className="page">
-      <main className="main">
-        <Image
-          className="logo"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="intro">
-          <h1>To get started, edit the page.tsx file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="ctas">
-          <a
-            className="primary"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="logo"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="secondary"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-    */
-   
+  return ( 
    < div>
-     
 <h1>Welcome to Handcrafter's Haven!</h1>
     <p>Your one-stop shop for all things handmade and artisanal.</p>
     <h2>Featured Products</h2>
